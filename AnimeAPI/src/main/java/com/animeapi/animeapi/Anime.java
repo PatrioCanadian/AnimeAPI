2ca8e95dca23c8912e1d2d1018521ce8887eafae
PatrioCanadian <christothegamer79@gmail.com>
2025-04-20 15:55:56 -0400

    Adds interactive menu to Anime application
    Implements an interactive menu for users to choose between listing all animes or searching by name.
    This enhances the user experience by providing options for interacting with the Anime API.

--- AnimeAPI/src/main/java/com/animeapi/animeapi/Anime.java
+++ AnimeAPI/src/main/java/com/animeapi/animeapi/Anime.java
@@ -1,6 +1,7 @@
 package com.animeapi.animeapi;
 
 import com.animeapi.animeapi.payload.AnimePayload;
+import com.fasterxml.jackson.databind.ext.SqlBlobSerializer;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -16,19 +17,39 @@ public class Anime extends Application {
     @Override
     public void start(Stage primaryStage) {
         AnimeAPI API_Anime = new AnimeAPI();
-        Scanner ConsoleReader = new Scanner(System.in);
-        System.out.print("Entrer le nom de l'anime: ");
-        String AnimeName = ConsoleReader.nextLine();
 
-        try{
-            List<AnimePayload> ListAnime = API_Anime.GetAnimeByName(AnimeName);
-            for(AnimePayload anime : ListAnime){
-                System.out.println(anime.titlename);
-            }
+        Scanner ConsoleReader = new Scanner(System.in);
+        System.out.println("1. Liste de tous les animes");
+        System.out.println("2. Obtenir anime par nom");
+        int Choix = Integer.parseInt(ConsoleReader.nextLine());
+        switch (Choix) {
+
+            case 1:
+                try {
+                    List<AnimePayload> ListAnime = API_Anime.GetAnimes();
+                    for (AnimePayload anime : ListAnime) {
+                        System.out.println(anime.titlename);
+                    }
+                } catch (Exception Error) {
+                    System.out.println(Error.getMessage());
+                }
+                break;
+            case 2:
+                System.out.print("Entrer le nom de l'anime: ");
+                String AnimeName = ConsoleReader.nextLine();
+                try {
+                    List<AnimePayload> ListAnime = API_Anime.GetAnimeByName(AnimeName);
+                    for (AnimePayload anime : ListAnime) {
+                        System.out.println(anime.titlename);
+                    }
+                } catch (Exception Error) {
+                    System.out.println(Error.getMessage());
+                }
+                break;
+
+
         }
-        catch (Exception Error){
-            System.out.println(Error.getMessage());
-        }
+        
 
     }
 }
